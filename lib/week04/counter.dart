0e7ddf56c371ef5d97d408fa4d7377e2b7fdff12
WUWSW <jakkawat667@gmail.com>
2025-09-04 15:59:00 +0700

    V.1

--- lib/week04/counter.dart
+++ lib/week04/counter.dart
@@ -9,26 +9,33 @@ class MyWidget extends StatefulWidget {
 
 class _MyWidgetState extends State<MyWidget> {
   // State variable
-  String message = 'Hello';
+  // String message = 'Hello';
+  int count = 0;
+  // methods
+  void update() {
+    setState(() {
+      // message = (message == 'Hello') ? 'Hi' : 'Hello';
+      count++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('counter')),
-      body: Center(child: Text(message)), // Display the state variable
+      appBar: AppBar(
+        title: Text('counter', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue[500],
+      ),
+      body: Center(
+        child: Text(
+          'Count = $count',
+          style: TextStyle(fontSize: 25, color: Colors.green[900]),
+        ),
+      ), // Display the state variable
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // setState(() {
-          //   message = 'Button clicked!';
-          // });
-
-          // debugPrint(message);
-
-          setState(() {
-           message = (message == 'Hello') ? 'Hi' : 'Hello';
-          });
-        },
-        child: Text('Click'),
+        onPressed: update,
+        backgroundColor: Colors.blue,
+        child: Text('Click', style: TextStyle(color: Colors.white)),
       ),
     );
   }
